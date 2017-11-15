771baf9261766b2a9fd1786dfd0c592bc0a4bfe1
Danny Haslund <dannyhasl1@gmail.com>
2017-11-15 09:35:06 +0100

    Initilazation of program

---
+++ ConnectCisco/ConnectCisco/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectCisco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerialPort stream = new SerialPort();
+
+            stream.PortName = "COM3";
+            stream.BaudRate = 9600;
+            stream.DataBits = 8;
+            stream.Parity = Parity.None;
+            stream.StopBits = StopBits.One;
+
+            stream.Open();
+            stream.WriteLine("enable");
+            stream.WriteLine("conf t");
+            stream.WriteLine("banner motd #Welcome Authorized Users Unauthorized access prohibited!# ");
+            stream.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
